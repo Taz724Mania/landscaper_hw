65122e76195b4a017df3c37fec1acc42c26866da
Shelby Campbell <shelbyacampbell2724@gmail.com>
2023-11-15 19:55:09 -0500

    userCanUseTeethToCutGrass

---
+++ app.js
@@ -0,0 +1,2 @@
+// You are starting a landscaping business, but all you have are your teeth.
+// Using just your teeth, you can spend the day cutting lawns and make $1. You can do this as much as you want.
