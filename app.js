b42887b638a082cfcaa0ef8a87478a218dd95fba
Shelby Campbell <shelbyacampbell2724@gmail.com>
2023-11-15 20:03:24 -0500

    userCanUseTheTools

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 // You are starting a landscaping business, but all you have are your teeth.
 // Using just your teeth, you can spend the day cutting lawns and make $1. You can do this as much as you want.
+
+let money = 0;
+let currentTool = 'teeth';
+
+function cutLawns() {
+  if (currentTool === 'teeth') {
+    money += 1;
+  }  } else if (currentTool === 'rustyScissors') {
+    money += 5;
+}
